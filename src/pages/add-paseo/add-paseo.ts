fb48a4ada8395b3480a1f58dd5172e7c5d5b3ff9
Valentina <martaelenagrane@Air-de-Marta.fibertel.com.ar>
2020-03-19 01:07:41 -0300

    form paseo

--- src/pages/add-paseo/add-paseo.ts
+++ src/pages/add-paseo/add-paseo.ts
@@ -17,7 +17,23 @@ import { HomePage } from '../home/home';
 export class AddPaseoPage {
 
   message = true;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  selected = 1;
+  dayOpcSelected = 1;
+  // diasSeleccionados = [1,0,0,0,0,0,0];
+  daysWeek;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams)
+  {
+    this.daysWeek =
+    [
+      { Id:0, Name:'Lun', Selec:1 },
+      { Id:1, Name:'Mar', Selec:1 },
+      { Id:2, Name:'Mié', Selec:0 },
+      { Id:3, Name:'Jue', Selec:1 },
+      { Id:4, Name:'Vie', Selec:1 },
+      { Id:5, Name:'Sab', Selec:0 },
+      { Id:6, Name:'Dom', Selec:0 },
+    ]
   }
 
   ionViewDidLoad() {
@@ -34,4 +50,28 @@ export class AddPaseoPage {
     this.navCtrl.setRoot(HomePage);
   }
 
+  changePetSelected(number)
+  {
+    this.selected = number;
+  }
+
+  changeOpcDaySelected(number)
+  {
+    this.dayOpcSelected = number;
+  }
+
+  selectDay(id)
+  {
+    debugger;
+    var day = this.daysWeek.find(x=>x.Id==id);
+    var index = this.daysWeek.indexOf(day);
+    if(this.daysWeek[index].Selec == 1)
+    {
+      this.daysWeek[index].Selec = 0;
+    }
+    else
+    {
+      this.daysWeek[index].Selec = 1;
+    }
+  }
 }
